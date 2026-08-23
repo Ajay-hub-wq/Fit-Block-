0e717a8c644d4ff3fbfabed2cd5bf9c3711c7e38
Ajay-hub-wq <ckck3044@gmail.com>
2026-08-23 12:45:06 +0530

    Update build.gradle.kts

--- Fit Block !/app/build.gradle.kts
+++ Fit Block !/app/build.gradle.kts
@@ -27,10 +27,16 @@ android {
     composeOptions { kotlinCompilerExtensionVersion = "1.5.4" }
 }
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui:1.5.4")
     implementation("androidx.compose.ui:ui-graphics:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.compose.foundation:foundation:1.5.4")
     implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
 }
