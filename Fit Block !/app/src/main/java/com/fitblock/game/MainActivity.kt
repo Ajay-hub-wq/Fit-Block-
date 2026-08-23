1d6e0bbdb3a696f6c5e4e6f02cfb7fa8f32f6902
Ajay-hub-wq <ckck3044@gmail.com>
2026-08-23 14:01:34 +0530

    Simplify setContent by removing theme wrapper
    Removed FitBlockTheme wrapper from setContent.

--- Fit Block !/app/src/main/java/com/fitblock/game/MainActivity.kt
+++ Fit Block !/app/src/main/java/com/fitblock/game/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import com.fitblock.game.ui.GameViewModel
 import com.fitblock.game.ui.screens.FitBlockGameRoot
-import com.fitblock.game.ui.theme.FitBlockTheme
 
 class MainActivity : ComponentActivity() {
     private val viewModel: GameViewModel by viewModels()
@@ -14,9 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FitBlockTheme {
-                FitBlockGameRoot(viewModel = viewModel)
-            }
+            FitBlockGameRoot(viewModel = viewModel)
         }
     }
 }
