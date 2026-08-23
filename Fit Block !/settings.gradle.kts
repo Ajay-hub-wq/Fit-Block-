01af2ccb5d61af809c16accc56efaa8690d659d2
Ajay-hub-wq <ckck3044@gmail.com>
2026-08-23 12:16:20 +0530

    Rename root project and update included module

--- Fit Block !/settings.gradle.kts
+++ Fit Block !/settings.gradle.kts
@@ -1,4 +1,3 @@
-
 pluginManagement {
     repositories {
         google()
@@ -6,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,5 +13,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "FitBlock"
-include(":app")
+
+rootProject.name = "Fit Block"
+include(":Fit Block !")
